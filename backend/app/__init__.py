2e242f25160ba950b84cbca33bd5f2df1be56c11
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-08-27 23:57:00 +0500

    shifting back to supabase

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
-from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 load_dotenv()
 
-app.config['MONGO_URI'] = os.getenv('DATABASE_URL')
-
-mydb = PyMongo(app)
+app.config['MONGO_URI'] = os.getenv('DATABASE_URI')
 
 
 from app import routes
